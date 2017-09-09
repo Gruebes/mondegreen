ea9934f8d66c918c32f6e6145ec2c0473e94f6be
TannerGruebele <tanner@saambaa.com>
2017-09-09 12:16:06 -0700

    Added auth components and routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Navbar, NavItem } from 'react-materialize';
-import Welcome from './Components/Welcome'
 import './App.css';
+
+import Welcome from './Components/Welcome';
+import Signup from './Components/Signup';
+import Signin from './Components/Signin';
+
+
 
 const App = () =>
 <Router>
   <div>
       <Navbar brand='Mondegreen' right>
         <NavItem><Link to="/">Home</Link></NavItem>
-        <NavItem><Link to="/">Profile</Link></NavItem>
+        <NavItem><Link to="/signup">Signup</Link></NavItem>
+        <NavItem><Link to="/signin">Signin</Link></NavItem>        
       </Navbar>
-      <Route exact path='/' component={Welcome} />        
+      <Route exact path='/' component={Welcome} /> 
+      <Route exact path='/signup' component={Signup} />        
+      <Route exact path='/signin' component={Signin} />        
   </div>
 </Router>;
 
