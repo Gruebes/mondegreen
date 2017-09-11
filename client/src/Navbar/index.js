46f21662110c8d05339ca4db4d6f1797377bce1b
TannerGruebele <tanner@saambaa.com>
2017-09-10 23:40:25 -0700

    debugging logout

--- client/src/Navbar/index.js
+++ client/src/Navbar/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
+import { logoutUser } from '../actions';
 
+console.log(logoutUser)
 
 export default class Navbar extends Component {
     state = { activeItem: 'home' }
     
       handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+      handleLogout = () => {
+        logoutUser()
+      }
     
       render() {
         const { activeItem } = this.state
@@ -18,6 +24,8 @@ export default class Navbar extends Component {
               <Menu.Item name='dashboard' active={activeItem === 'dashboard'} onClick={this.handleItemClick} ><Link to="/dashboard">dashboard</Link></Menu.Item>              
               <Menu.Item name='register' active={activeItem === 'register'} onClick={this.handleItemClick} ><Link to="/register">register</Link></Menu.Item>
               <Menu.Item name='login' active={activeItem === 'login'} onClick={this.handleItemClick} ><Link to="/login">login</Link></Menu.Item>
+              <Menu.Item name='logout' active={activeItem === 'login'} onClick={this.handleLogout.bind(this)} ></Menu.Item>
+              
               </Menu>
           </Segment>
         )
